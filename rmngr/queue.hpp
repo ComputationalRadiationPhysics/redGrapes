cd5bdd2b1ab10bbc391246be8e58b941d9113791
Michael Sippel <m.sippel@hzdr.de>
2018-03-04 22:32:27 +0100

    scheduling graph

--- rmngr/queue.hpp
+++ rmngr/queue.hpp
@@ -8,63 +8,24 @@
 namespace rmngr
 {
 
-template <typename T, typename ReadyMarker>
+template <typename ID>
 class Queue
 {
     public:
-        typedef std::size_t ID;
+        virtual ~Queue() {}
 
-        Queue(ReadyMarker const& mark_ready_)
-            : id_counter(0), mark_ready(mark_ready_)
-        {}
-
-        virtual ~Queue()
-        {}
-
-        ID push(T* a)
+        virtual void push(ID a)
         {
-            ID id = ++this->id_counter;
-            this->objects[id] = std::unique_ptr<T>(a);
-            this->push_(id);
-            this->queue.insert(this->queue.begin(), id);
-            this->update_ready(id);
-            return id;
+            this->queue.insert(this->queue.begin(), a);
         }
 
-        void finish(ID id)
+        virtual void finish(ID a)
         {
-            this->finish_(id);
-            this->queue.erase(std::find(this->queue.begin(), this->queue.end(), id));
-            this->objects.erase(id);
-        }
-
-        T& operator[] (ID id)
-        {
-            return *this->objects[id];
+            this->queue.erase(std::find(this->queue.begin(), this->queue.end(), a));
         }
 
     protected:
         std::list<ID> queue;
-
-        virtual void push_(ID id) {}
-        virtual void finish_(ID id) {}
-
-        virtual bool is_ready(ID id)
-        {
-            return true;
-        }
-
-        void update_ready(ID id)
-        {
-            if(this->is_ready(id))
-                this->mark_ready(id);
-        }
-
-    private:
-        std::map<ID, std::unique_ptr<T>> objects;
-        ID id_counter;
-
-        ReadyMarker mark_ready;
 };
 
 } // namespace rmngr
