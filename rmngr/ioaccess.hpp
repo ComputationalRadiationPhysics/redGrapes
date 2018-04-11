33eb6cb1c93f10d34ed412db52e19e4924c9649c
Michael Sippel <m.sippel@hzdr.de>
2018-04-11 21:57:38 +0200

    adapt IOAccess & IOResource

--- rmngr/ioaccess.hpp
+++ rmngr/ioaccess.hpp
@@ -1,42 +1,74 @@
 
+/**
+ * @file rmngr/ioaccess.hpp
+ */
+
 #pragma once
 
+#include <boost/graph/adjacency_matrix.hpp>
+
 #include <rmngr/dependency_manager.hpp>
+#include <rmngr/resource.hpp>
 
 namespace rmngr
 {
 
 struct IOAccess
 {
-    enum Id
+    enum
     {
         root,
         read,
         write,
         aadd,
         amul,
-    };
+    } mode;
 
-    template <typename DependencyManager>
-    static void addOperation(DependencyManager& m, Id const& id, bool atomic)
+    static bool
+    is_serial(
+        IOAccess a,
+        IOAccess b
+    )
     {
-        m.add_vertex(id);
-        m.add_dependency(root, id);
-        m.add_dependency(id, root);
-        if(! atomic)
-            m.add_dependency(id, id);
+        using Graph = boost::adjacency_matrix<boost::undirectedS>;
+        struct Initializer
+        {
+            void operator() (Graph& g) const
+            {
+                // atomic operations
+                boost::add_edge(root, read, g);
+                boost::add_edge(root, aadd, g);
+                boost::add_edge(root, amul, g);
+
+                // non-atomic
+                boost::add_edge(root, write, g);
+                boost::add_edge(write, write, g);
+            };
+        }; // struct Initializer
+
+        static StaticDependencyManager<
+            Graph,
+            Initializer,
+            5
+        > const m;
+        return m.is_serial(a.mode, b.mode);
     }
 
-    template <typename DependencyManager>
-    static void build_dependencies(DependencyManager& m)
-    {
-        m.add_vertex(root);
-        addOperation(m, read, true);
-        addOperation(m, write, false);
-        addOperation(m, aadd, true);
-        addOperation(m, amul, true);
-    }
 }; // struct IOAccess
+
+struct IOResource : public Resource< IOAccess >
+{
+#define OP(name) \
+  inline ResourceAccess name (void) \
+  { return this->make_access(IOAccess{IOAccess::name}); }
+
+  OP(read)
+  OP(write)
+  OP(aadd)
+  OP(amul)
+
+#undef OP
+}; // struct IOResource
 
 } // namespace rmngr
 
