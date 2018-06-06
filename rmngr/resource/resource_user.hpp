fce684fff4737f2882560db15913ccea2e85cb74
Michael Sippel <micha@infinitycoding.de>
2018-06-06 10:51:28 +0200

    ResourceUser: is_superset_of()

--- rmngr/resource/resource_user.hpp
+++ rmngr/resource/resource_user.hpp
@@ -13,34 +13,43 @@ namespace rmngr
 
 class ResourceUser
 {
-    public:
-        ResourceUser()
-        {}
+  public:
+    ResourceUser() {}
 
-        ResourceUser(std::vector<ResourceAccess> const& access_list_)
-          : access_list(access_list_)
-        {}
+    ResourceUser( std::vector<ResourceAccess> const & access_list_ )
+        : access_list( access_list_ )
+    {
+    }
 
-        static bool
-        is_serial(
-            ResourceUser const & a,
-            ResourceUser const & b
-        )
+    static bool
+    is_serial( ResourceUser const & a, ResourceUser const & b )
+    {
+        for ( ResourceAccess const & ra : a.access_list )
+            for ( ResourceAccess const & rb : b.access_list )
+                if ( ResourceAccess::is_serial( ra, rb ) )
+                    return true;
+        return false;
+    }
+
+    bool
+    is_superset_of( ResourceUser const & a ) const
+    {
+        for ( ResourceAccess const & ra : a.access_list )
         {
-            for(ResourceAccess const& ra : a.access_list)
-            {
-                for(ResourceAccess const& rb : b.access_list)
-                {
-                    if(ResourceAccess::is_serial(ra, rb))
-                        return true;
-                }
-            }
-            return false;
-        }
+            bool found = false;
+            for ( ResourceAccess const & r : this->access_list )
+                if ( r.is_same_resource( ra ) && r.is_superset_of( ra ) )
+                    found = true;
 
-        //protected:
-        std::vector<ResourceAccess> access_list;
+            if ( !found )
+                // a introduced a new resource
+                return false;
+        }
+        return true;
+    }
+
+    // protected:
+    std::vector<ResourceAccess> access_list;
 }; // class ResourceUser
 
 } // namespace rmngr
-
