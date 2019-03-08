a0b51886c119a694d92f5fcabefb8d9b3034c8c5
Michael Sippel <micha@infinitycoding.de>
2019-03-08 22:41:13 +0100

    refactor, document & test ThreadDispatcher

--- rmngr/thread_dispatcher.hpp
+++ rmngr/thread_dispatcher.hpp
@@ -17,12 +17,36 @@ namespace thread
 static thread_local size_t id;
 }
 
+/**
+ * @defgroup Thread
+ * @{
+ * @par Required public member functions
+ * - constructor: `Thread(Callable, Args&&...)` spawns a new thread which executes the callable with the given arguments
+ * - `void join()`
+ * @}
+ */
+
+/**
+ * @defgroup Selector
+ * @{
+ * @par Description
+ * Provides the ThreadDispatcher with Jobs.
+ * A returned job must be a nullary functor returning void, i.e. of type `void _()`.
+ * The public functions are required to be threadsafe.
+ *
+ * @par Required public member functions
+ * - `bool empty()` shouldn't return true until no job will come after
+ * - `Job getJob()` returns the next job. If none is available it should return a job that does nothing (or waits until new jobs are ready, if you want to avoid busy waiting)
+ *
+ * @}
+ */
+
 /** Manages a thread pool.
  * Worker-threads request jobs from scheduler and execute them,
  * until the ThreadDispatcher gets destroyed and all workers finished.
  *
- * @tparam Scheduler needs lockfree Callable getJob(int id) and bool empty()
- * @tparam Thread must create a thread on construction and have join().
+ * @tparam Selector provides the jobs which get executed, required to implement the concept @ref Selector
+ * @tparam Thread must satisfy @ref Thread
  */
 template <typename Selector, typename Thread = std::thread>
 class ThreadDispatcher
@@ -48,30 +72,61 @@ class ThreadDispatcher
 
     Selector & selector;
     std::vector<Worker> workers;
-    std::atomic_bool running;
+    std::atomic_int n_working;
 
   public:
-    ThreadDispatcher( Selector & selector_, size_t n_threads )
-        : selector( selector_ ), running( true )
+    /**
+     * @param selector the job selector
+     * @param n_threads number of worker threads which will get spawed additionally to the main thread.
+     */
+    ThreadDispatcher( Selector & selector, size_t n_threads )
+         : selector( selector ), n_working( 1 + n_threads  )
     {
+        // set id for main thread
         thread::id = 0;
+
+	// create worker threads
         for ( size_t i = 1; i <= n_threads; ++i )
             this->workers.emplace_back( this, i );
     }
 
+    /**
+     * Get the next job from the selector and execute it
+     */
     void
     consume_job( void )
     {
         (this->selector.getJob())();
     }
 
+    /**
+     * Consume jobs until **all** threads finished (i.e. selector returns empty)
+     */
     void
     work( void )
     {
-        while( ! this->selector.empty() )
-            this->consume_job();
+        bool working = true;
+        while( this->n_working > 0 )
+	{
+            if( this->selector.empty() )
+	    {
+	        if( working )
+		{
+		    working = false;
+		    -- this->n_working;
+		}
+	    }
+	    else
+	    {
+	        if( !working )
+		{
+		    working = true;
+		    ++ this->n_working;
+		}
+	    }
 
-        this->selector.finish();
+            this->consume_job();
+	}
     }
 
     ~ThreadDispatcher()
