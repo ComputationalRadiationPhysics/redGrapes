5f7a77ff46b1d6a0cdc1aec76f0f9695b26e12b6
Michael Sippel <m.sippel@hzdr.de>
2019-06-09 23:39:56 +0200

    add optional library

--- rmngrConfig.cmake
+++ rmngrConfig.cmake
@@ -3,5 +3,9 @@ cmake_minimum_required(VERSION 3.10.0)
 
 include(CMakeFindDependencyMacro)
 
+target_include_directories(rmngr SYSTEM INTERFACE
+    $<BUILD_INTERFACE:${rmngr_SOURCE_DIR}/share/thirdParty/akrzemi/optional/include>
+)
+
 set(rmngr_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}")
 
