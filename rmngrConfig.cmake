85c9ae9f8e806275606a668c51738db675db2c9d
Michael Sippel <m.sippel@hzdr.de>
2019-06-10 15:37:51 +0200

    cmake config: fix include dirs

--- rmngrConfig.cmake
+++ rmngrConfig.cmake
@@ -3,9 +3,5 @@ cmake_minimum_required(VERSION 3.10.0)
 
 include(CMakeFindDependencyMacro)
 
-target_include_directories(rmngr SYSTEM INTERFACE
-    $<BUILD_INTERFACE:${rmngr_SOURCE_DIR}/share/thirdParty/akrzemi/optional/include>
-)
-
 set(rmngr_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}")
-
+set(rmngr_INCLUDE_DIRS ${rmngr_INCLUDE_DIRS} "${CMAKE_CURRENT_LIST_DIR}/share/thirdParty/akrzemi/optional/include")
