27254d71d59e2b12d3c4afbf95a5f7d74941765b
Michael Sippel <micha@infinitycoding.de>
2019-06-15 12:09:05 +0200

    refactor delayed functor wrapping

---
+++ rmngr/delayed_functor.hpp
@@ -0,0 +1,55 @@
+
+#pragma once
+
+#include <future>
+#include <type_traits>
+
+namespace rmngr
+{
+
+template < typename NullaryCallable >
+class DelayedFunctor
+{
+private:
+    using Result = typename std::result_of<NullaryCallable()>::type;
+
+public:
+    DelayedFunctor(NullaryCallable && impl)
+        : impl(std::move(impl)) {}
+
+    void operator() (void)
+    {
+        set_promise(this->result, this->impl);
+    }
+
+    std::future<Result> get_future(void)
+    {
+        return this->result.get_future();
+    }
+
+private:
+    NullaryCallable impl;
+    std::promise<Result> result;
+
+    template <typename T, typename F>
+    static void set_promise (std::promise<T>& p, F& func)
+    {
+        p.set_value(func());
+    }
+
+    template <typename F>
+    static void set_promise (std::promise<void>& p, F& func)
+    {
+        func();
+        p.set_value();
+    }
+}; // class DelayedFunctor
+
+template< typename NullaryCallable >
+auto make_delayed_functor( NullaryCallable && f )
+{
+    return DelayedFunctor< NullaryCallable >(std::move(f));
+}
+
+} // namespace rmngr
+
