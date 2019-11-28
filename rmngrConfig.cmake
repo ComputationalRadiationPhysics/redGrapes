f567029c934ca858f471fe571f77bff780cdf844
Michael Sippel <m.sippel@hzdr.de>
2019-11-28 02:42:07 +0100

    cmake files: update project name

--- rmngrConfig.cmake
+++
@@ -1,7 +0,0 @@
-
-cmake_minimum_required(VERSION 3.10.0)
-
-include(CMakeFindDependencyMacro)
-
-set(rmngr_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}")
-set(rmngr_INCLUDE_DIRS ${rmngr_INCLUDE_DIRS} "${CMAKE_CURRENT_LIST_DIR}/share/thirdParty/akrzemi/optional/include")
