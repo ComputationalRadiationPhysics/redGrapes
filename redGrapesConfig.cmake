cbc7ef85b812e970fc99237d15fc25708634afc2
Michael Sippel <micha@infinitycoding.de>
2019-11-28 02:42:07 +0100

    cmake files: update project name

---
+++ redGrapesConfig.cmake
@@ -0,0 +1,7 @@
+
+cmake_minimum_required(VERSION 3.10.0)
+
+include(CMakeFindDependencyMacro)
+
+set(redGrapes_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}")
+set(redGrapes_INCLUDE_DIRS ${redGrapes_INCLUDE_DIRS} "${CMAKE_CURRENT_LIST_DIR}/share/thirdParty/akrzemi/optional/include")
