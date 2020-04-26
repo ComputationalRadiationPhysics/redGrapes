45beca3642abb5777c2bd383b085061eac652d59
Michael Sippel <m.sippel@hzdr.de>
2020-04-27 01:59:55 +0200

    fix task continuations

--- redGrapes/task/task.hpp
+++ redGrapes/task/task.hpp
@@ -19,12 +19,14 @@ namespace redGrapes
 
 struct TaskImplBase
 {
+    virtual ~TaskImplBase() {};
     virtual void run() = 0;
 
     void operator() ()
     {
         if( ! resume_cont )
         {
+            finished = false;
             resume_cont = boost::context::callcc(
                 [this]( boost::context::continuation && c )
                 {
@@ -38,32 +40,36 @@ struct TaskImplBase
                     for( auto & f : after_hooks )
                         f();
 
+                    finished = true;
+
                     return std::move( this->yield_cont );
                 }
             );
         }
         else
         {
-            this->resume_cont = this->resume_cont->resume_with([this](auto && c)
-                                           {
-                                               this->yield_cont = std::move( c );
-                                               for( auto & f : resume_hooks )
-                                                 f();
+            for( auto & f : resume_hooks )
+                f();
 
-                                               return std::move( this->yield_cont );
-                                           });
+            resume_cont = resume_cont->resume();
+        }
+
+        if( ! finished )
+        {
+            for( auto & f : pause_hooks )
+                f( event_id );
         }
     }
 
     void yield( unsigned int event_id )
     {
-        this->yield_cont = this->yield_cont.resume_with([this, event_id](auto && c){
-                                         for( auto & f : pause_hooks )
-                                             f( event_id );
-                                         return std::move(c);
-                                     });
+        this->event_id = event_id;
+        yield_cont = yield_cont.resume();
     }
 
+    bool finished;
+    unsigned int event_id;
+    
     std::vector<std::function<void()>> before_hooks;
     std::vector<std::function<void()>> after_hooks;
 
@@ -83,6 +89,8 @@ struct FunctorTask : TaskImplBase
     FunctorTask( NullaryCallable && impl )
         : impl( std::move(impl) )
     {}
+
+    ~FunctorTask(){}
 
     void run()
     {
