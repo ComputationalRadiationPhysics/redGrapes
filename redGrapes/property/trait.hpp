490e5102cfeda558b870801a1a8ac03a50f5817d
Michael Sippel <m.sippel@hzdr.de>
2021-02-04 01:27:36 +0100

    impl BuildProperties for reference_wrapper

--- redGrapes/property/trait.hpp
+++ redGrapes/property/trait.hpp
@@ -1,4 +1,5 @@
 
+
 #pragma once
 
 #include <typeinfo>
@@ -18,6 +19,18 @@ struct BuildProperties
     static void build(Builder & builder, T const & t)
     {
         spdlog::warn("trait `redGrapes::BuildProperties` is not implemented for {}", typeid(T).name());
+    }
+};
+
+template <
+    typename T
+>
+struct BuildProperties< std::reference_wrapper< T > >
+{
+    template <typename Builder>
+    static void build(Builder & builder, std::reference_wrapper< T > const & t)
+    {
+        builder.add( t.get() );
     }
 };
 
