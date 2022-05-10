f12f321fab4c3319add0e2407eaeb45f466b2da0
Michael Sippel <m.sippel@hzdr.de>
2022-05-10 17:50:44 +0200

    cmake config: make link libraries and include directories public

--- redGrapesConfig.cmake
+++ redGrapesConfig.cmake
@@ -9,7 +9,7 @@ find_package(Boost 1.62.0 REQUIRED COMPONENTS context)
 find_package(fmt REQUIRED)
 find_package(spdlog REQUIRED)
 
-add_library(redGrapes STATIC
+add_library(redGrapes
   ${CMAKE_CURRENT_LIST_DIR}/redGrapes/resource/resource.cpp
   ${CMAKE_CURRENT_LIST_DIR}/redGrapes/dispatch/thread/execute.cpp
   ${CMAKE_CURRENT_LIST_DIR}/redGrapes/scheduler/event.cpp
@@ -20,21 +20,23 @@ add_library(redGrapes STATIC
   ${CMAKE_CURRENT_LIST_DIR}/redGrapes/redGrapes.cpp
 )
 
-target_compile_features(redGrapes INTERFACE
+target_compile_features(redGrapes PUBLIC
     cxx_std_14
 )
-  
-target_include_directories(redGrapes INTERFACE
+
+target_include_directories(redGrapes PUBLIC
     $<BUILD_INTERFACE:${redGrapes_SOURCE_DIR}>
     $<INSTALL_INTERFACE:${redGrapes_INSTALL_PREFIX}>
 )
 
-target_link_libraries(redGrapes INTERFACE ${Boost_LIBRARIES})
-target_link_libraries(redGrapes INTERFACE fmt::fmt)
-target_link_libraries(redGrapes INTERFACE spdlog::spdlog)
+target_link_libraries(redGrapes PUBLIC ${Boost_LIBRARIES})
+target_link_libraries(redGrapes PUBLIC fmt::fmt)
+target_link_libraries(redGrapes PUBLIC spdlog::spdlog)
 
 set(redGrapes_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}")
 set(redGrapes_INCLUDE_DIRS ${redGrapes_INCLUDE_DIRS} "${CMAKE_CURRENT_LIST_DIR}/share/thirdParty/akrzemi/optional/include")
 set(redGrapes_INCLUDE_DIRS ${redGrapes_INCLUDE_DIRS} "${CMAKE_CURRENT_LIST_DIR}/share/thirdParty/cameron314/concurrentqueue/include")
 set(redGrapes_LIBRARIES ${Boost_LIBRARIES} fmt::fmt spdlog::spdlog)
+
+target_include_directories(redGrapes PUBLIC ${redGrapes_INCLUDE_DIRS})
 
