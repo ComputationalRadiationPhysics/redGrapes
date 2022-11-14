9e8e5d44113172d021585a4fd36d250676fa1272
Michael Sippel <m.sippel@hzdr.de>
2022-11-14 23:15:35 +0100

    scheduler: implement atomic bitsets

--- redGrapes/scheduler/default_scheduler.hpp
+++ redGrapes/scheduler/default_scheduler.hpp
@@ -4,7 +4,6 @@
 #include <pthread.h>
 #include <thread>
 #include <condition_variable>
-#include <bitset>
 
 #include <redGrapes/task/task_space.hpp>
 #include <redGrapes/scheduler/scheduler.hpp>
@@ -21,17 +20,15 @@ namespace scheduler
 struct DefaultScheduler : public IScheduler
 {
     CondVar cv;
-
-    std::mutex m;
     task::Queue ready;
-
-
     unsigned n_workers;
 
-    //!  true if worker available, false if worker busy
-    std::vector<std::bitset<64>> worker_state;
+    //!  bit is true if worker available, false if worker busy
+    std::array< std::atomic< uint64_t >, 8> worker_state;
 
     std::vector<std::shared_ptr< dispatch::thread::WorkerThread >> threads;
+
+    std::atomic< unsigned > last_free;
 
     DefaultScheduler( size_t n_threads = std::thread::hardware_concurrency() )
         : n_workers( n_threads )
@@ -39,20 +36,12 @@ struct DefaultScheduler : public IScheduler
         for( size_t i = 0; i < n_threads; ++i )
         {
             threads.emplace_back(std::make_shared< dispatch::thread::WorkerThread >( i ));
-
             cpu_set_t cpuset;
             CPU_ZERO(&cpuset);
             CPU_SET(i, &cpuset);
             int rc = pthread_setaffinity_np(threads[i]->thread.native_handle(), sizeof(cpu_set_t), &cpuset);
-
-            // initially , every worker is available
-            unsigned j = i / 64;
-            if( j >= worker_state.size() )
-                worker_state.emplace_back();
-
-            worker_state[j].set( i % 64 );
         }
-        
+
         // if not configured otherwise,
         // the main thread will simply wait
         redGrapes::idle =
@@ -75,34 +64,58 @@ struct DefaultScheduler : public IScheduler
             worker->stop();
     }
 
-    void set_worker_free( unsigned id )
+    inline void free_worker( unsigned id )
     {
-        worker_state[ id / 64 ].set( id % 64 );
+        //assert( id < n_workers );
+        worker_state[ id / 64 ].fetch_or( (uint64_t)1 << ( id % 64 ), std::memory_order_release);
+        last_free.store(id, std::memory_order_release);
     }
-    
+
+    inline bool alloc_worker( unsigned id )
+    {
+        unsigned j = id / 64;
+        unsigned k = id % 64;
+
+        uint64_t old_val = worker_state[j].fetch_and(~((uint64_t)1 << k), std::memory_order_acquire);
+        return old_val & ((uint64_t)1 << k);
+    }
+
     /*
      * try to find an available worker
      *
      * @return worker_id if found free worker,
      *         -1 if all workers are busy
      */
-    int find_free_worker()
+    inline int alloc_worker()
     {
-        std::lock_guard< std::mutex > lock(m);
+        unsigned off = last_free.fetch_add(1, std::memory_order_acquire) % n_workers;
+        unsigned joff = off / 64;
+        unsigned koff = off % 64;
 
-        unsigned j = 0;
-        for( std::bitset<64> & s : worker_state )
+        for(uint64_t j0 = 0; j0 < worker_state.size(); ++j0)
         {
-            if( s.any() )
+            unsigned j = (j0 + joff) % worker_state.size();
+            if( worker_state[j].load() != 0 )
             {
-                for( unsigned i = 0; i < 64; ++i )
-                    if( s.test(i) )
+                unsigned end = 64;
+                if( (j+1) == (n_workers/64) )
+                    end = n_workers%64;
+
+                for(uint64_t k0 = 0; k0 < end; ++k0)
+                {
+                    unsigned k = (k0 + koff) % end;
+
+                    unsigned i = j*64 + k;
+                    if( i < n_workers )
                     {
-                        s.reset(i);
-                        return j * 64 + i;
+                        uint64_t old_val = worker_state[j].fetch_and(~((uint64_t)1 << k), std::memory_order_acquire);
+                        if( old_val & ((uint64_t)1 << k) )
+                            return i;
                     }
+                    else
+                        return -1;
+                }
             }
-            j++;
         }
 
         return -1;
@@ -112,16 +125,14 @@ struct DefaultScheduler : public IScheduler
     {
         SPDLOG_TRACE("DefaultScheduler::activate_task({})", task.task_id);
 
-        int worker_id = find_free_worker();
+        int worker_id = alloc_worker();
 
-        if( worker_id >= 0 )
+        if( worker_id < 0 )
+            ready.push(&task);
+        else
         {
             threads[ worker_id ]->queue.push(&task);
             threads[ worker_id ]->wake();
-        }
-        else
-        {
-            ready.push(&task);
         }
     }
 
@@ -129,32 +140,46 @@ struct DefaultScheduler : public IScheduler
     // @return true if a new task was assigned to worker
     bool schedule( dispatch::thread::WorkerThread & worker )
     {
-        SPDLOG_TRACE("schedule worker {}", (void*)&worker);
+        SPDLOG_TRACE("schedule worker {}", worker.id);
+
         while( true )
         {
             Task *t = nullptr;
 
+            // try to initialize a new task
             if( t = ready.pop() )
             {
                 worker.queue.push(t);
+                SPDLOG_TRACE("got task from ready queue");
                 return true;
             }
 
-            // try to initialize a new task
-            if( top_space->init_dependencies( t, true ) )
+            unsigned i = 2;
+            while( i-- > 0 )
             {
-                if( t )
+                if( top_space->init_dependencies(t, true) )
                 {
-                    // the newly initialized task is ready
-                    worker.queue.push(t);
-                    return true;
+                    if( t )
+                    {
+                        // the newly initialized task is ready
+                        SPDLOG_TRACE("found task in space");
+                        worker.queue.push(t);
+                        return true;
+                    }
                 }
-            }
-            else
-            {
-                // emplacement queue is empty
-                set_worker_free( worker.id );
-                return false;
+                else
+                {
+                    // emplacement queue is empty
+                    if( t = ready.pop() )
+                    {
+                        worker.queue.push(t);
+                        SPDLOG_TRACE("got task from ready queue");
+                        return true;
+                    }
+
+                    free_worker( worker.id );
+                    return false;
+                }
             }
         }
     }
@@ -163,15 +188,14 @@ struct DefaultScheduler : public IScheduler
     {
         SPDLOG_DEBUG("DefaultScheduler: wake_one_worker()");
 
-        int worker_id = find_free_worker();
-        if( worker_id >= 0 )
+        int worker_id = alloc_worker();
+
+        if( worker_id < 0 )
+            return false;
+        else
         {
             threads[ worker_id ]->wake();
             return true;
-        }
-        else
-        {
-            return false;
         }
     }
 
