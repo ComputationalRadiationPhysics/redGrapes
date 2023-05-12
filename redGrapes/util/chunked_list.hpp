f18a37b3b2ecad76f2da0a43a6eea20e8659b1f3
Michael Sippel <m.sippel@hzdr.de>
2023-05-12 13:21:39 +0200

    rework ChunkedList

--- redGrapes/util/chunked_list.hpp
+++ redGrapes/util/chunked_list.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2022 Michael Sippel
+/* Copyright 2022-2023 Michael Sippel
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -17,28 +17,69 @@
 #include <memory>
 #include <optional>
 #include <redGrapes/util/allocator.hpp>
+#include <redGrapes/util/spinlock.hpp>
+#include <redGrapes/util/trace.hpp>
 #include <spdlog/spdlog.h>
 
 namespace redGrapes
 {
 
-/*
- * A lock-free, append/remove container.
+/*!
+ * TODO: Name?
+ * ALRARAC - Allocation & Lock Reducing Append/Remove Array Container
+ * LARAC- Lock- & Allocation-Reducing Array Container
  *
- * New elements can only be `push`ed to the end,
- * but can not be inserted at random position.
- * Depending on `chunk_size` 
+ * This container class supports two basic mutable operations,
+ * both of which can be performed **concurrently** and with nearly **constant time**:
+ *    - *push()*: append an element at the end, returns its index
+ *    - *remove()*: deletes the element given its index
  *
- * Elements are removed in constant time,
- * however their memory is still occupied
- * until all elements of the chunk are removed.
+ * It is implemented as two-Level pointer tree where
+ * the first level is a dynamicly sized array, called *SuperChunk*.
+ * This *SuperChunk* references a growing number of *Chunks*,
+ * a staticly-sized array each.
  *
  *    [ ChkPtr_1, ChkPtr_2, .., ChkPtr_chunk_count ]
  *        |           |_________________     |_____
  *       V                              V          V
  *    [ x_1, x_2, .., x_chunk_size ] [ ... ] .. [ ... ]
  *
+ * New elements can only be `push`ed to the end,
+ * but can not be inserted at random position.
+ * Depending on `chunk_size` , adding new elements
+ * is performed in constant time and without allocations,
+ * as long as the chunk still has capacity.
+ * Only one of chunk_size many calls of push() require
+ * memory allocation.
  *
+ * Elements are removed in constant time and
+ * removed elements are skipped by the iterators,
+ * however their memory is still occupied
+ * until all elements of the chunk are removed.
+ *
+ * Iteration can be started at a specific index and
+ * proceed in forward direction (++) aswell as reversed (--).
+ *
+ * Indices returned by push() can be used with begin_from() / remove().
+ * These indices are static, i.e. they stay valid after other remove() operations.
+ *
+ *
+ * ## Example Usecases:
+ *
+ *  - **Resource User List** (exist per resource) is used concurrently with:
+ *     - push() from mostly one but potentially many task-creator-threads through emplace_task(),
+ *     - reversed iteration and remove() from Worker threads
+ *       from task.init_graph() and task.remove_from_resources().
+ *
+ *  - **Event Follower List** (exists per event):
+ *     - push() concurrently by multiple Workers initializing new task dependenies,
+ *     - remove() concurrently my mutliple Workers through update_graph().
+ *
+ *  - **Access List** (exists per task):
+ *     - push() from only one single thread that is initializing the task
+ *     and after that finished,
+ *     - remove() from only one single thread.
+ *     - concurrently to the first two, all Worker threads may iterate read-only.
  */
 template < typename T >
 struct ChunkedList
@@ -46,69 +87,176 @@ struct ChunkedList
 private:
     size_t const chunk_size;
 
-    using Chunk = std::optional<T> * ;
-    Chunk * chunks;
+    struct Item
+    {
+        struct TrivialInit_t{};
+        union ItemStorage
+        {
+            unsigned char dummy;
+            T value;
 
-    std::atomic< uint16_t > chunks_capacity;
-    std::atomic< uint16_t > chunks_count;
+            ItemStorage( TrivialInit_t ) noexcept
+                : dummy()
+            {}
+
+            template < typename... Args >
+            ItemStorage( Args&&... args )
+                : value(std::forward<Args>(args)...)
+            {}
+
+            ~ItemStorage(){}
+        };
+
+        std::atomic< uint8_t > refcount;
+        ItemStorage storage;
+
+        Item()
+            : refcount( 0 )
+            , storage( TrivialInit_t{} )
+        {
+        }
+
+        T & operator=(T const & value)
+        {
+            if( refcount.fetch_add(1) == 0 )
+            {
+                storage.value = value;
+                return storage.value;
+            }
+            else
+                throw std::runtime_error("assign item which is in use");
+        }
+    };
+
+    struct ItemAccess
+    {
+    private:
+        friend class ChunkedList;
+
+        bool has_item;
+        Item & item;
+
+        ItemAccess( Item & item )
+            : item(item)
+        {
+            if( item.refcount > 0 )
+            {
+                item.refcount++;
+                has_item = true;
+            }
+            else
+                has_item = false;
+        }
+
+    public:
+        ItemAccess( ItemAccess const & other )
+            : ItemAccess( other.item )
+        {
+        }
+
+        ~ItemAccess()
+        {
+            if( has_item )
+                item.refcount--;
+        }
+
+        bool is_some() const
+        {
+            return has_item;
+        }
+
+        T& operator* () const
+        {
+            assert( is_some() );
+            return item.storage.value;
+        }
+    };
+    
+    using Chunk = Item *;
+
+    std::atomic< uint16_t > superchunk_size;
+    std::atomic< uint16_t > superchunk_capacity;
+    Chunk * superchunk;
+
     std::atomic< uint32_t > next_item_id;
+    std::atomic< uint32_t > size_;
 
+    memory::Allocator< Item >  item_alloc;
+    memory::Allocator< Chunk > chunk_alloc;
+
+    // 
+    SpinLock m;
+
+    // m is locked whenever this function is called
     void init_chunk( )
     {
-        unsigned chunk_idx = chunks_count.fetch_add(1);
-
-        if( chunk_idx >= chunks_capacity )
-            resize_superchunk( chunks_capacity * 2 );
-
-        std::optional<T>* new_chunk = memory::Allocator<std::optional<T>>().allocate(chunk_size);
+        Item * new_chunk = item_alloc.allocate(chunk_size);
         for( int i = 0; i < chunk_size; ++i )
-            new_chunk[i] = std::nullopt;
+            new (&new_chunk[i]) Item;
 
-        chunks[ chunk_idx ] = new_chunk;
+        unsigned chunk_idx = superchunk_size;
+
+        if( chunk_idx >= superchunk_capacity )
+            resize_superchunk( superchunk_capacity + 8 );
+
+        superchunk[ chunk_idx ] = new_chunk;
+
+        superchunk_size ++;
     }
 
-    void resize_superchunk( size_t new_chunks_capacity )
+    void resize_superchunk( size_t new_superchunk_capacity )
     {
-        assert( new_chunks_capacity > chunks_capacity );
+        assert( new_superchunk_capacity > superchunk_capacity );
 
-        SPDLOG_TRACE("resize to {} chunks", new_chunks_capacity);
+        SPDLOG_TRACE("resize superchunk to {} chunks", new_superchunk_capacity);
 
-        Chunk * new_chunks = memory::Allocator<Chunk >().allocate( new_chunks_capacity );
+        // TODO: Optimization
+        //  In the case that the allocated memory region
+        //  for the new superchunk directly follows the old one,
+        //  we dont need to copy the elements, just 'merge' the two
+        //  and set the superchunk_capacity appropriately.
+        // Question: Where to store old pointer to free it later?
+        // Trick: ChunkAllocator only cares about count of free()'s, not the pointer
 
-        for( int i = 0; i < chunks_capacity; ++i )
-            new_chunks[i] = chunks[i];
-        for( int i = chunks_capacity; i < new_chunks_capacity; ++i )
-            new_chunks[i] = nullptr;
+        Chunk * new_superchunk = chunk_alloc.allocate( new_superchunk_capacity );
 
-        auto old_chunks = chunks;
-        chunks = new_chunks;
+        for( int i = 0; i < superchunk_capacity; ++i )
+            new_superchunk[i] = superchunk[i];
 
-        memory::Allocator< Chunk >().deallocate( old_chunks, chunks_capacity );
-        chunks_capacity = new_chunks_capacity;
+        for( int i = superchunk_capacity; i < new_superchunk_capacity; ++i )
+            new_superchunk[i] = nullptr;
+
+        auto old_superchunk = superchunk;
+        superchunk = new_superchunk;
+
+        chunk_alloc.deallocate( old_superchunk, superchunk_capacity );
+        superchunk_capacity = new_superchunk_capacity;
     }
 
     public:
         ~ChunkedList()
         {
-            for( unsigned i = 0; i < chunks_count; ++i )
-                if( chunks[i] )
-                    memory::Allocator< std::optional<T> >().deallocate( chunks[i], chunk_size );
-            memory::Allocator<Chunk >().deallocate( chunks, chunks_capacity );
+            for( unsigned i = 0; i < superchunk_size; ++i )
+                if( superchunk[i] )
+                    item_alloc.deallocate( superchunk[i], chunk_size );
+
+            chunk_alloc.deallocate( superchunk, superchunk_capacity );
         }
 
         ChunkedList( size_t chunk_size = 256 )
             : chunk_size( chunk_size )
             , next_item_id( 0 )
-            , chunks_count( 0 )
-            , chunks_capacity( 16 )
+            , size_( 0 )
+            , superchunk_size( 0 )
+            , superchunk_capacity( 16 )
         {
-            chunks = memory::Allocator< Chunk >().allocate( chunks_capacity );
-            for( int i = 0; i < chunks_capacity; ++i )
-                chunks[i] = nullptr;
+            superchunk = chunk_alloc.allocate( superchunk_capacity );
+            for( int i = 0; i < superchunk_capacity; ++i )
+                superchunk[i] = nullptr;
         }
 
         ChunkedList( ChunkedList && other ) = default;
-        
+
         ChunkedList( ChunkedList const& other )
             : ChunkedList()
         {
@@ -120,12 +268,12 @@ private:
 
         unsigned size() const noexcept
         {
-            return next_item_id;
+            return size_;
         }
 
         unsigned capacity() const noexcept
         {
-            return chunks_count * chunk_size;
+            return superchunk_size * chunk_size;
         }
 
         unsigned free_capacity() const noexcept
@@ -145,54 +293,50 @@ private:
 
         void reserve_chunks( size_t n )
         {
-            SPDLOG_TRACE("reserve {} chunks", n);
+            //spdlog::info("reserve {} chunks", n);
 
-            if( chunks_count + n > chunks_capacity )
-                resize_superchunk( chunks_count + n );
+            std::unique_lock< SpinLock > l(m);
 
-            assert( chunks_count + n <= chunks_capacity );
+            if( superchunk_size + n > superchunk_capacity )
+                resize_superchunk( superchunk_size + n );
+
+            assert( superchunk_size + n <= superchunk_capacity );
 
             for( size_t i = 0; i < n; ++i )
                 init_chunk();
         }
 
-        std::optional< T > & get( unsigned idx )
+        ItemAccess get( unsigned idx ) const
         {
             unsigned chunk_idx = idx / chunk_size;
             unsigned chunk_off = idx % chunk_size;
 
-            assert( chunk_idx < chunks_count );
+            assert( chunk_idx < superchunk_size );
 
-            if( chunks[ chunk_idx ] )
-                return chunks[ chunk_idx ][ chunk_off ];
+            if( superchunk[ chunk_idx ] )
+                return ItemAccess( superchunk[ chunk_idx ][ chunk_off ] );
             else
-                throw std::runtime_error("invalid index: missing chunk");
-        }
-
-        std::optional< T > const & get( unsigned idx ) const
-        {
-            unsigned chunk_idx = idx / chunk_size;
-            unsigned chunk_off = idx % chunk_size;
-
-            assert( chunk_idx < chunks_count );
-
-            if( chunks[ chunk_idx ] )
-                return chunks[ chunk_idx ][ chunk_off ];
-            else
-                throw std::runtime_error("invalid index: missing chunk");
+                throw std::runtime_error(fmt::format("invalid index: missing chunk {}", chunk_idx));
         }
 
         unsigned push( T const& item )
-        {            
+        {
+            TRACE_EVENT("ChunkedList", "push");
             unsigned idx = next_item_id.fetch_add(1);
             unsigned chunk_idx = idx / chunk_size;
             unsigned chunk_off = idx % chunk_size;
 
-            while( chunk_idx >= chunks_capacity
-                || chunks[ chunk_idx ] == nullptr )
-                init_chunk();
+            {
+                // TODO: reduce this critical section
+                std::unique_lock< SpinLock > l(m);
 
-            chunks[ chunk_idx ][ chunk_off ] = item;
+                while( chunk_idx >= superchunk_capacity
+                       || superchunk[ chunk_idx ] == nullptr )
+                    init_chunk();
+
+                superchunk[ chunk_idx ][ chunk_off ] = item;
+                size_ ++;
+            }
 
             return idx;
         }
@@ -200,18 +344,21 @@ private:
         void remove(unsigned idx)
         {
             assert( idx < size() );
-            get( idx ) = std::nullopt;
+            get(idx).item.refcount--;
         }
 
         void erase( T item )
         {
             for( unsigned i = 0; i < size(); ++i )
             {
-                auto & x = get(i);
-                if( x )
+                ItemAccess x = get(i);
+                if( x.is_some() )
                 {
                     if( *x == item )
+                    {
                         remove( i );
+                        //return;
+                    }
                 }
             }
         }
@@ -260,9 +407,14 @@ private:
             bool is_some() const
             {
                 if( idx < c.size() && idx >= 0 )
-                    return (bool) (c.get(idx));
+                    return c.get(idx).is_some();
                 else
                     return false;
+            }
+
+            bool is_empty() const
+            {
+                return c.size() == 0;
             }
 
             T const & operator*() const
@@ -312,12 +464,12 @@ private:
             bool is_some() const
             {
                 if( idx < c.size() && idx >= 0 )
-                    return (bool) (c.get(idx));
+                    return c.get(idx).is_some();
                 else
                     return false;
             }
 
-            T& operator*() const
+            T & operator*() const
             {
                 assert( is_some() );
                 return *c.get(idx);
@@ -326,12 +478,59 @@ private:
 
         auto begin_from(unsigned idx)
         {
+            if( size() == 0 )
+                return end();
+
             auto it = Iterator{ *this, (int)idx };
 
-            if( ! it.is_some() )
+            while( ! it.is_some() && it != end() )
+                ++it;
+
+            return it;
+        }
+
+        auto begin_from(unsigned idx) const
+        {
+            if( size() == 0 )
+                return end();
+
+            auto it = ConstIterator{ *this, (int)idx };
+
+            while( ! it.is_some() && it != end() )
+                ++it;
+
+            return it;
+        }
+    
+        auto begin_from_rev(unsigned idx)
+        {
+            if( size() == 0 )
+                return end();
+
+            auto it = Iterator{ *this, (int)idx };
+
+            while( ! it.is_some() && it != end() )
                 --it;
 
             return it;
+        }
+
+        auto begin_from_rev(unsigned idx) const
+        {
+            if( size() == 0 )
+                return end();
+
+            auto it = ConstIterator{ *this, (int)idx };
+
+            while( ! it.is_some() && it != end() )
+                --it;
+
+            return it;
+        }
+    
+        auto begin() const
+        {
+            return begin_from(0);
         }
 
         auto begin()
@@ -344,35 +543,11 @@ private:
             return Iterator{ *this, -1 };
         }
 
-        auto begin_from(unsigned idx) const
-        {
-            auto it = ConstIterator{ *this, (int)idx };
-
-            if( ! it.is_some() )
-                --it;
-
-            return it;
-        }
-        
-        auto begin() const
-        {
-            return begin_from(0);
-        }
-
         auto end() const
         {
             return ConstIterator{ *this, -1 };
         }
 
-        auto iter()
-        {
-            return std::make_pair(begin(), end());
-        }
-
-        std::pair<Iterator, Iterator> iter_from(unsigned idx)
-        {
-            return std::make_pair(begin_from(idx), end());
-        }
     };
 
 } // namespace redGrapes
--- test/chunked_list.cpp
+++ test/chunked_list.cpp
@@ -1,9 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
+#include <thread>
 
 #include <redGrapes/util/chunked_list.hpp>
 
-TEST_CASE("chunked list")
+TEST_CASE("ChunkedList singlethreaded")
 {
     redGrapes::ChunkedList< unsigned > l( 32 );
 
@@ -15,13 +16,13 @@ TEST_CASE("chunked list")
     for( auto & x : l ) {
         REQUIRE( false );
     }
-
+    
     // size remains 0 while capacity increases
     l.reserve( 4096 );
     REQUIRE( l.size() == 0 );
     REQUIRE( l.capacity() >= 4096 );
     REQUIRE( l.free_capacity() >= 4096 );
-
+    
     // empty iterator should still not iterate
     for( auto & x : l ) {
         REQUIRE( false );
@@ -29,15 +30,17 @@ TEST_CASE("chunked list")
 
     // insert elements
     for(unsigned i=0; i < 4096; ++i)
+    {
         l.push(i);
+    }
 
     // reversed iterator
     for(unsigned j=0; j < 4096; ++j)
     {
         unsigned i = j;
-        for(auto it=l.iter_from(i); it.first != it.second; --it.first)
+        for(auto it=l.begin_from(i); it != l.end(); --it)
         {
-            REQUIRE( *it.first == i );
+            REQUIRE( *it == i );
             i--;
         }
     }
@@ -47,19 +50,38 @@ TEST_CASE("chunked list")
     unsigned r2 = 48;
     unsigned r3 = 49;
     unsigned r4 = 1023;
-
+    
     l.remove(r1);
     l.remove(r2);
     l.remove(r3);
     l.remove(r4);
 
-    // check that iterator skips removed elements
+    // check that forward iterator skips removed elements
+    unsigned i = 0;
+    for(auto it = l.begin(); it != l.end(); --it)
+    {
+        unsigned x = *it;
+        if( i == r4 ) i++;
+        if( i == r3 ) i++;
+        if( i == r2 ) i++;
+        if( i == r1 ) i++;
+
+        REQUIRE( x == i );
+        i++;
+    }
+    
+    // check that reversed iterator skips removed elements
     for(unsigned j=1; j < 4096; ++j)
     {
         unsigned i = j;
-        for(auto it = l.iter_from(j); it.first != it.second; --it.first)
+
+        // dont start from removed elements
+        if( i == r1 || i == r2 || i == r3 || i == r4 ) break;
+
+        // check reversed iterator
+        for(auto it = l.begin_from_rev(i); it != l.end(); --it)
         {
-            unsigned x = *it.first;
+            unsigned x = *it;
             if( i == r4 ) i--;
             if( i == r3 ) i--;
             if( i == r2 ) i--;
@@ -69,5 +91,153 @@ TEST_CASE("chunked list")
             i--;
         }
     }
+}
+
+TEST_CASE("ChunkedList: push || push")
+{
+    redGrapes::ChunkedList< int > l( 8 );
+
+    unsigned end = 20000;
+    unsigned mid = end / 2;
+
+    auto t1 = std::thread([&l, mid] {
+        for( int i = 0; i < mid; ++i )
+            l.push(i);
+    });
+
+    auto t2 = std::thread([&l, mid, end] {
+        for( int i = mid; i < end; ++i )
+            l.push(i);
+    });
+
+    t1.join();
+    t2.join();
+
+    int expect1 = 0;
+    int expect2 = mid;
+
+    for( int x : l )
+    {
+        if( x < mid )
+            REQUIRE( x == expect1++ );
+        else
+            REQUIRE( x == expect2++ );
+    }
+
+    REQUIRE( expect1 == mid );
+    REQUIRE( expect2 == end );
+}
+
+TEST_CASE("ChunkedList: remove || remove")
+{
+    redGrapes::ChunkedList< int > l( 8 );
+    for( int i = 0; i < 20000; ++i )
+        l.push(i);
+
+    auto t1 = std::thread([&l] {
+        for( int i = 0; i < 10000; ++i )
+            l.remove(i*2);
+    });
+
+    auto t2 = std::thread([&l] {
+        for( int i = 0; i < 10000; ++i )
+            l.remove(i*2+1);
+    });
+
+    t1.join();
+    t2.join();
+
+    REQUIRE( l.size() == 20000 );
+
+    // empty iterator
+    for( int x : l )
+        REQUIRE(false);
+}
+
+TEST_CASE("ChunkedList: push || remove")
+{
+    redGrapes::ChunkedList< int > l( 8 );
+
+    // start with 10k elements
+    for( int i = 0; i < 10000; ++i )
+        l.push(i);
+
+    // push another 10k elements
+    auto t1 = std::thread([&l] {
+        for( int i = 10000; i < 20000; ++i )
+            l.push(i);
+    });
+
+    // while removing the first 10k elements
+    auto t2 = std::thread([&l] {
+        for( int i = 0; i < 10000; ++i )
+            l.remove(i);
+    });
+
+    t1.join();
+    t2.join();
+
+    REQUIRE( l.size() == 20000 );
+
+    int expected = 10000;
+    for( int x : l )
+        REQUIRE( x == expected++ );
+
+    REQUIRE( expected == 20000 );
+}
+
+TEST_CASE("ChunkedList: push || iter")
+{
+    redGrapes::ChunkedList< int > l( 8 );
+
+    // start with 10k elements
+    for( int i = 0; i < 10000; ++i )
+        l.push(i);
+
+    // push another 10k elements
+    auto t1 = std::thread([&l] {
+        for( int i = 10000; i < 20000; ++i )
+            l.push(i);
+    });
+
+    // while iterating over all elements
+    auto t2 = std::thread([&l] {
+        int expected = 0;
+        for( int x : l )
+            REQUIRE( x == expected++ );
+
+        REQUIRE( expected >= 10000 );
+    });
+
+    t1.join();
+    t2.join();
+}
+
+TEST_CASE("ChunkedList: remove || iter")
+{
+    redGrapes::ChunkedList< int > l( 8 );
+
+    // start with 10k elements
+    for( int i = 0; i < 10000; ++i )
+        l.push(i);
+
+    // remove every other element
+    auto t1 = std::thread([&l] {
+        for( int i = 0; i < 10000; i += 2 )
+            l.remove(i);
+    });
+
+    // while iterating over all elements
+    auto t2 = std::thread([&l] {
+        int expected = 0;
+        for( int x : l )
+        {
+            REQUIRE( (x == expected || x == (expected+1)) );
+            expected = x + 1;
+        }
+    });
+
+    t1.join();
+    t2.join();
 }
 
