6bb76cce776adfe03e5821e9d84534ac461249b3
Michael Sippel <m.sippel@hzdr.de>
2023-08-23 19:43:31 +0200

    add cv test

---
+++ test/cv.cpp
@@ -0,0 +1,34 @@
+
+#include <catch2/catch_test_macros.hpp>
+#include <thread>
+#include <redGrapes/util/cv.hpp>
+
+TEST_CASE("CV")
+{
+    for(int i=0; i< 5000; ++i)
+    {
+        volatile bool finished = false;
+        volatile bool start = false;
+
+        redGrapes::CondVar cv;
+
+        std::thread t([&] {                
+            /* wait should run through without waiting,
+             * because notify came before wait
+             */
+            cv.wait();
+            finished = true;
+        });
+
+        cv.notify();
+        
+        auto end = std::chrono::steady_clock::now() + std::chrono::seconds(10);
+        while( std::chrono::steady_clock::now() < end )
+            if( finished )
+                break;
+        
+        REQUIRE( finished );
+        t.join();
+    }
+
+}
