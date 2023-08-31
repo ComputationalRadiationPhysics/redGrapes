872dc80200fd4a3b5a55967ea189f5ab111a31b8
Michael Sippel <m.sippel@hzdr.de>
2023-08-31 19:44:51 +0200

    task queued set block size to 16

--- redGrapes/task/queue.hpp
+++ redGrapes/task/queue.hpp
@@ -44,7 +44,7 @@ struct TaskQueueTraits
 	// but many producers, a smaller block size should be favoured. For few producers
 	// and/or many elements, a larger block size is preferred. A sane default
 	// is provided. Must be a power of 2.
-	static const size_t BLOCK_SIZE = 8;
+	static const size_t BLOCK_SIZE = 16;
 
 	// For explicit producers (i.e. when using a producer token), the block is
 	// checked for being empty by iterating through a list of flags, one per element.
