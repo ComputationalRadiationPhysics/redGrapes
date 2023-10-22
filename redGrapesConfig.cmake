07fd2ffeb7c6a13cb4cf126606108cc6a8bd8d11
Michael Sippel <m.sippel@hzdr.de>
2023-10-22 11:10:20 +0200

    add backward-cpp

--- redGrapesConfig.cmake
+++ redGrapesConfig.cmake
@@ -39,6 +39,9 @@ else()
   message(STATUS "Found hwloc")
 endif()
 
+set(Backward_DIR "${CMAKE_CURRENT_LIST_DIR}/share/thirdParty/bombela/backward-cpp")
+find_package(Backward)
+
 
 if( NOT TARGET redGrapes )
 add_library(redGrapes
@@ -73,6 +76,7 @@ target_link_libraries(redGrapes PUBLIC ${Boost_LIBRARIES})
 target_link_libraries(redGrapes PUBLIC fmt::fmt)
 target_link_libraries(redGrapes PUBLIC spdlog::spdlog)
 target_link_libraries(redGrapes PUBLIC ${HWLOC})
+target_link_libraries(redGrapes PUBLIC Backward::Backward)
 
 set(redGrapes_INCLUDE_DIRS ${redGrapes_CONFIG_INCLUDE_DIR} ${CMAKE_CURRENT_LIST_DIR})
 set(redGrapes_INCLUDE_DIRS ${redGrapes_INCLUDE_DIRS} "${CMAKE_CURRENT_LIST_DIR}/share/thirdParty/akrzemi/optional/include")
